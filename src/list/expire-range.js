f12e86b2dd39c407bcf326f2105470e62af94d55
Jorgen Evens <jorgen@evens.eu>
2018-08-18 10:39:41 +0200

    [expire] use options

--- src/list/expire-range.js
+++ src/list/expire-range.js
@@ -1,5 +1,6 @@
 import _applyRange from './_apply-range';
 import _expire from './_expire';
+import options from './options';
 
 import _list from './_list';
 import _expires from '../status/_expires';
@@ -14,10 +15,7 @@ function expireRange(root, listName, start, end, opts = {}) {
     const list = _list(root, listName);
 
     if (list.total > 0)
-        return list;
+        return root;
 
-    return {
-        ...list,
-        total: null
-    };
+    return options(root, listName, { total: null });
 }
