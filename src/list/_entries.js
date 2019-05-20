7b007b9f246117e8858c2af22f1d031bf0be1f97
Jorgen Evens <jorgen@evens.eu>
2019-05-20 23:49:06 +0200

    [list] verify resource state in entries
    Verify that the state of the list entry matches that of the resource it
    references. This would prevent a list from automatically updating when a
    resource was changed or expired.

--- src/list/_entries.js
+++ src/list/_entries.js
@@ -1,6 +1,16 @@
 import _total from './_total';
 import _list from './_list';
 import _expand from './_expand';
+import isState from '../status/is-state';
+import { OK } from '../constants';
+
+function _resourceState(root, id) {
+    if (!root || !root.resources || !root.resources[id])
+        return null;
+
+    const entry = root.resources[id];
+    return entry.state;
+}
 
 export default
 function _entries(root, listName, start, end) {
@@ -16,7 +26,16 @@ function _entries(root, listName, start, end) {
 
     const count = end - start;
     const result = entries
-        .slice(start, end);
+        .slice(start, end)
+        .map(entry => {
+            if (!isState(entry, OK))
+                return entry;
+
+            const state = _resourceState(root, entry && entry.id);
+            if (!state) return null;
+
+            return { ...entry, state };
+        });
 
     if (result.length == count)
         return result;
