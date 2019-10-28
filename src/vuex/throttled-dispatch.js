e9b6f48ccb75f8b99dfefc9a7087c52726896506
Jorgen Evens <jorgen@evens.eu>
2019-10-29 00:33:30 +0100

    [vuex][throttled-dispatch] Bundle leading edge calls

--- src/vuex/throttled-dispatch.js
+++ src/vuex/throttled-dispatch.js
@@ -1,7 +1,52 @@
-import _debounce from 'lodash/debounce';
 import hash from 'hash-it';
 
 const throttled = new Map();
+const run = (func, args) => (0, func)(...args);
+
+/**
+ * Debounce implementation that will group all calls made in
+ * the first 10ms into a single function call on the leading edge.
+ *
+ * Any calls past the initial 10ms will be debounced until `timeout`
+ * has expired, just like a normal debounce.
+ *
+ * After the trailing edge invocation the debounce method is reset
+ * and the flow resets to invoking the leading edge.
+ */
+function _debounce(func, timeout) {
+    let first = 0;
+    let result = null;
+    let timer = null;
+
+    return function(...args) {
+        const diff = Date.now() - first;
+
+        // Leading edge
+        if (!first || diff >= timeout) {
+            result = run(func, args);
+            first = Date.now();
+            return result;
+        }
+
+        // Do nothing if within 10ms from leading edge
+        if (diff < 10)
+            return result;
+
+        // New invocation, cancel previous invocation
+        if (timer)
+            clearTimeout(timer);
+
+        // Schedule new invocation
+        timer = setTimeout(() => {
+            result = run(func, args);
+            first = 0;
+            timer = null;
+        }, timeout);
+
+        // Return the result of the last invocation
+        return result;
+    };
+}
 
 function __debounce(func, timeout, opts) {
     const cache = {};
@@ -10,7 +55,7 @@ function __debounce(func, timeout, opts) {
         const key = opts.id(...args);
 
         if (!cache[key])
-            cache[key] = _debounce(func, timeout, opts);
+            cache[key] = _debounce(func, timeout);
 
         return (0, cache[key])(...args);
     };
@@ -22,7 +67,6 @@ function wrapDispatch(dispatch) {
         return throttled.get(dispatch);
 
     const wrapper = __debounce(dispatch, 1000, {
-        leading: true,
         id: (...args) => hash(args)
     });
 
