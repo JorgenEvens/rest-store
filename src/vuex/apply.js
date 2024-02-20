d2659dad549b2ff456ee060949ebb28a52a260ac
Jorgen Evens <jorgen@evens.eu>
2024-02-21 00:15:17 +0100

    fix(vuex): prevent updeep from freezing state
    When using update on state with an empty update object the state passed
    in might be freezed.
    This will make any attempt to `assign()` properties fail due to the
    initial state being frozen. Effectively making the vuex state fully
    immutable.
    
    This is solved by passing in a very superficial clone into any operation
    passed to the `apply()` function.

--- src/vuex/apply.js
+++ src/vuex/apply.js
@@ -1,6 +1,7 @@
 export default
 function apply(action, state, ...args) {
-    const next = action(state, ...args);
+    const root = state && Object.create(state);
+    const next = action(root, ...args);
 
     state = state || {};
     Object.assign(state, next);
